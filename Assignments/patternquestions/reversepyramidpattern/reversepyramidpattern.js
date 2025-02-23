f5cbec64fa967c94b9c2408eee89572a56fbb955
vipul attri <133794719+vipulattri@users.noreply.github.com>
2025-02-23 19:58:19 +0530

    Add files via upload

---
+++ Assignments/patternquestions/reversepyramidpattern/reversepyramidpattern.js
@@ -0,0 +1,17 @@
+let n = 5; 
+
+for (let i = n; i >= 1; i--) {
+    let row = "";
+
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+  
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
